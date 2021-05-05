4bb9a1b3b55e25b673a34ab2d4c1f8743287176c
David Raznick <kindly@gmail.com>
2021-05-05 18:57:05 +0100

    remove favicon

--- site/src/views/Home.vue
+++ site/src/views/Home.vue
@@ -73,7 +73,7 @@
           </v-card-title >
           <v-card-text >
             <p>Click on the output formats above to get the data!</p>
-            <p>The Colab Notebook is an online jupyter notebook and gives you a way to do custom analysis on the whole of IATI by getting the data in under 3mins.</p>
+            <p>The Colab Notebook is an online jupyter notebook and gives you a quick way to do custom analysis on the whole of IATI data. It contains some useage examples.</p>
           </v-card-text>
         </v-card>
       </v-col>
